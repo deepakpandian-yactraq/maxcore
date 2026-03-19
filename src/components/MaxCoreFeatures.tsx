f02fe3fd3771a2db37906b87d9b9c21d61e7578c
Deepak Pandian R <96948342+deepakpandianr@users.noreply.github.com>
2026-03-19 20:20:55 +0530

    Add justify-center class to feature grid

--- src/components/MaxCoreFeatures.tsx
+++ src/components/MaxCoreFeatures.tsx
@@ -41,7 +41,7 @@ export default function MaxCoreFeatures() {
           Key Features & Benefits
         </h2>
 
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
           {features.map((item, index) => (
             <div
               key={index}
